66c6a255cdcfc0328e79a85262d3128bf29a5267
albrinstaf <alexander.br@kpigroups.com>
2020-02-27 14:16:45 +0200

    finished FormConstructor

--- Homework2/FormConstructor.php
+++ Homework2/FormConstructor.php
@@ -1,53 +1,93 @@
 <?php
 
-class FormConstructor
+
+interface FormConstructorInterface
+{
+    public function render(): string;
+}
+
+
+class FormConstructor implements FormConstructorInterface
 {
     private $fields = [];
 
+    function __construct() {
+        //print "Создан объект от " . __CLASS__  . "\n";
+    }
 
     public function __set($name, $value)
     {
-        // TODO: Implement __set() method.
+        $this->fields[$name] = $value;
     }
 
-    public function __get()
+    public function __get($name): string
     {
-
+        return '__get метод - ' . $this->fields[$name]  . "\n";
     }
 
-    public function render()
+    public function render(): string
     {
-        $formHtml = '<form>';
+        $formHtml = '<form>' . "\n";
         foreach ($this->fields as $name=>$value)
         {
-            $formHtml . = '<input type="text" name=>';
+            $formHtml .= '<input type="text" name="' . $name . '" value="' . $value . '" />' . "\n";
         }
-        $formHtml .= '</form>';
+        $formHtml .= '</form>' . "\n";
+
+        return $formHtml;
     }
 
-    public function __toString()
+    public function __toString(): string
     {
-        // TODO: Implement __toString() method.
-        return '';
+        $text = '';
+        foreach ($this->fields as $name=>$value)
+        {
+            $text .= $name . "=>" . $value . "\n";
+        }
+
+        echo ' __toString() метод.' . "\n";
+        return $text;
     }
+
+    public function __destruct()
+    {
+        //print "Уничтожены объекты " . __CLASS__  . "\n";
+    }
+
+    public function __invoke()
+    {
+       echo '__invoke() метод.' . "\n";
+       var_dump($this->fields);
+    }
+
 }
 
 
 $formConstructor = new FormConstructor();
-$formConstructor->first_name = 'Your first name';
+$formConstructor->first_name = 'Your first name'; //вызов метода __set
 $formConstructor->last_name = 'Your last name';
 $formConstructor->email = 'Your email';
 $formConstructor->address = 'Your address';
 
+//echo ($formConstructor->email); //вызов метода __get
+//echo($formConstructor); //вызов метода __toString
+//$formConstructor(); //вызов метода __invoke
+//echo $formConstructor->render(); //вызов метода render
+
+?>
 
 
-echo $formConstructor->render();
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
 
-
-
-/*
-<form>
-<input type="text" name="propertyName" value="propertyValue" />
-
-</form>
-*/
+    <?php echo $formConstructor->render(); ?>
+</body>
+</html>
